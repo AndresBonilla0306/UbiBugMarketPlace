eb419ce1caec440cf1f86c169793f35292b4ef8b
Camillo Ortegon <juan.ortegon@uao.edu.co>
2024-05-26 20:12:45 -0500

    Feat: Home and Products front end

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import SecondaryHeader from '../components/SecondaryHeader';
 import axios from 'axios';
+import '../css/Product.css';
 
 const ProductDetails = () => {
   const { id } = useParams();
@@ -21,17 +22,19 @@ const ProductDetails = () => {
   }, [id]);
 
   if (!product) {
-    return <div>Cargando...</div>;
+    return <div className="product-container">Cargando...</div>;
   }
 
   return (
-    <div>
-      <SecondaryHeader/>
-      <h1>Detalles del Producto {product.id}</h1>
-      <h1>Nombre: {product.name}</h1>
-      <h2>Descripción: {product.description}</h2>
-      <h2>Precio: {product.price}$</h2>
-      <h2>Rareza: {product.rarity}</h2>
+    <div className="product-container">
+      <SecondaryHeader className="header"/>
+      <div className="product-details">
+        <h1>Detalles del Producto {product.id}</h1>
+        <h1>Nombre: {product.name}</h1>
+        <h2>Descripción: {product.description}</h2>
+        <h2>Precio: {product.price}$</h2>
+        <h2>Rareza: {product.rarity}</h2>
+      </div>
     </div>
   );
 };
